2c1640319ffb5b9cfbf8ce3558bf3520750d6f90
崔大师 <1046166@qq.com>
2021-03-06 13:49:14 +0800

    更新支持OKchain

--- src/entities/token.ts
+++ src/entities/token.ts
@@ -65,5 +65,6 @@ export const WETH = {
   [ChainId.HECOMAIN]: new Token(ChainId.HECOMAIN, '0x5545153ccfca01fbd7dd11c0b23ba694d9509a6f', 18, 'WHT', 'Wrapped HT'),
   [ChainId.HECOTEST]: new Token(ChainId.HECOTEST, '0x50b29a42c9e4d0f940292c386207f492458ee358', 18, 'WHT', 'Wrapped HT'),
   [ChainId.BIANMAIN]: new Token(ChainId.BIANMAIN, '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c', 18, 'WBNB', 'Wrapped BNB'),
-  [ChainId.BIANTEST]: new Token(ChainId.BIANTEST, '0xae13d989dac2f0debff460ac112a837c89baa7cd', 18, 'WBNB', 'Wrapped BNB')
+  [ChainId.BIANTEST]: new Token(ChainId.BIANTEST, '0xae13d989dac2f0debff460ac112a837c89baa7cd', 18, 'WBNB', 'Wrapped BNB'),
+  [ChainId.OKTEST]: new Token(ChainId.BIANTEST, '0x3d6ed85b2c310c6a2e821ccf5b00ef38b2df2702', 18, 'WOKT', 'Wrapped OKT')
 }
